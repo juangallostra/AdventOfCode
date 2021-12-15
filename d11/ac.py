c60e66b8aba1e80b35e47a36e27e7f8df8d43c48
Juan Gallostra <juangallostra@gmail.com>
2021-12-15 16:33:37 +0100

    Add day 11 part 1

---
+++ d11/ac.py
@@ -0,0 +1,98 @@
+from utils import parse_input
+
+DAY = 11
+
+# Process:
+# 1. Update energy levels
+# 2. Flash octopus
+# While there was a flash in the last iteration:
+#   3. Update energy levels - skip already flashed and mark them as processed
+#   4. Flash octopus
+# 5. Count flashes
+# 6. Repeat from 1
+
+# easier to handle extremes if data is padded
+
+
+def pad_data(data):
+    padded = [[0 for _ in range(len(data[0])+2)]]
+    for row in data:
+        padded.append([0] + row + [0])
+    return padded + [[0 for _ in range(len(data[0])+2)]]
+
+
+def unpad_data(data):
+    return [[i for i in row[1:-1]] for row in data[1:-1]]
+
+
+def update_octopus(data):
+    return [[i+1 for i in row] for row in data]
+
+
+def flash_octopus(data):
+    return [[0 if i > 9 else i for i in row] for row in data]
+
+
+def count_flashes(data):
+    return sum([1 for row in data for i in row if i == -1])
+    pass
+
+
+def update_after_flash(data):
+    # octopus with a neighbour == 0 get a unit increase in energy
+    # octopus with a 0 -> -1 to mark as processed
+    padded_data = pad_data(data)
+    for x in range(len(data[0])):
+        for y in range(len(data)):
+            if data[y][x] == 0:  # the octopus flashed
+                # increase neighbours energy if they haven't flashed
+                coords = list(
+                    filter(
+                        lambda z: z != (x, y),
+                        zip(
+                            [x-1, x, x+1]*3,
+                            sorted([y-1, y, y+1]*3)
+                        )
+                    )
+                )
+                for coord in coords:
+                    # 0 flashed in this iteration, -1 if flashed in previous iterations
+                    if padded_data[coord[1]+1][coord[0]+1] not in [0, -1]:
+                        padded_data[coord[1]+1][coord[0]+1] += 1
+    # replace 0 by -1 to avoid processing twice
+    return unpad_data(padded_data)
+    # return [[-1 if i == 0 else i for i in row] for row in unpad_data(padded_data)]
+
+
+def part1(data):
+    num_steps = 100
+    data = [[int(i) for i in row] for row in data]
+    total_flashes = 0
+    for _ in range(num_steps):
+        data = update_after_flash(flash_octopus(update_octopus(data)))
+        chain_reaction = True
+        while chain_reaction:
+            # Mark octopus that have already flashed in this step
+            data = [[-1 if i == 0 else i for i in row] for row in data]
+            data = update_after_flash(flash_octopus(data))
+            # Check if new octopus have flashed in the reaction
+            if not 0 in [i for row in data for i in row]:
+                chain_reaction = False
+        # Update num of flashes
+        total_flashes += count_flashes(data)
+        data = [[0 if i==-1 else i for i in row] for row in data]
+    return total_flashes
+
+
+def part2(data):
+    pass
+
+
+def main(input_file):
+    data = parse_input(input_file)
+    print(f'Part1: {part1(data)}')
+    print(f'Part2: {part2(data)}')
+
+
+if __name__ == '__main__':
+    main(f'd{DAY}/data/input.txt')
---
+++ d11/tests.py
@@ -0,0 +1,96 @@
+
+import unittest
+from d11.ac import part1, part2, update_octopus, flash_octopus, update_after_flash, pad_data, unpad_data
+from utils import parse_input
+
+DAY = 11
+
+
+class AOCTests(unittest.TestCase):
+    def test_pad_data(self):
+        # Given
+        data = [[0, 2],
+                [9, 1]]
+                
+        output = [[0,0,0,0], [0, 0, 2, 0],
+                  [0, 9, 1, 0], [0,0,0,0]]
+        # When
+        result = pad_data(data)
+        # Then
+        self.assertListEqual(result, output)
+
+    def test_unpad_data(self):
+        # Given                
+        data = [[0,0,0,0], [0, 0, 2, 0],
+                  [0, 9, 1, 0], [0,0,0,0]]
+        
+        output = [[0, 2],
+                [9, 1]]
+        # When
+        result = unpad_data(data)
+        # Then
+        self.assertListEqual(result, output)
+        
+    def test_increase_energy(self):
+        # Given
+        data = [[0, 2],
+                [9, 1]]
+        output = [[1, 3],
+                  [10, 2]]
+        # When
+        result = update_octopus(data)
+        # Then
+        self.assertListEqual(result, output)
+
+    def test_flash_octoupus(self):
+        # Given
+        data = [[0, 2],
+                [10, 1]]
+        output = [[0, 2],
+                  [0, 1]]
+        # When
+        result = flash_octopus(data)
+        # Then
+        self.assertListEqual(result, output)
+
+    def test_update_after_flash(self):
+        # Given
+        data_1 = [[0, 2],
+                [7, 1]]
+        output_1 = [[0, 3],
+                  [8, 2]]
+        data_2 = [[0, 2],
+                  [7, 0]]
+        output_2 = [[0, 4],
+                    [9, 0]]
+
+        # When
+        result = update_after_flash(data_1)
+        result_2 = update_after_flash(data_2)
+        # Then
+        self.assertListEqual(result, output_1)
+        self.assertListEqual(result_2, output_2)
+
+    def test_part1(self):
+        # Given
+        input = parse_input(f'd{DAY}/data/test_input.txt')
+        expected_sol = parse_input(
+            f'd{DAY}/data/test_sol_1.txt', to_int=True, single_value=True)
+        # When
+        result = part1(input)
+        # Then
+        self.assertEqual(expected_sol, result)
+
+    # def test_part2(self):
+    #     # Given
+    #     input = parse_input(f'd{DAY}/data/test_input.txt')
+    #     expected_sol = parse_input(
+    #         f'd{DAY}/data/test_sol_2.txt', to_int=True, single_value=True)
+    #     # When
+    #     result = part2(input)
+    #     # Then
+    #     self.assertEqual(expected_sol, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
