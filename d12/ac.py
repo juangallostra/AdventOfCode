5872da250bc4035dc8a864fa02a179fc7d45ea7b
Juan Gallostra <juangallostra@gmail.com>
2021-12-16 08:18:08 +0100

    Day 12 layout

---
+++ d12/ac.py
@@ -0,0 +1,22 @@
+from utils import parse_input
+
+DAY = 12
+
+# All paths from start to end. Big caves (CAPS) can be visited more than once, small caves only once.
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+def main(input_file):
+    data = parse_input(input_file)
+    print(f'Part1: {part1(data)}')
+    print(f'Part2: {part2(data)}')
+
+
+if __name__ == '__main__':
+    main(f'd{DAY}/data/input.txt')
