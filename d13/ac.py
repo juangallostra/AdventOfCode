243f37982ed558d4a6b774e8001094b9bf151cdb
Juan Gallostra <juangallostra@gmail.com>
2021-12-16 09:44:49 +0100

    Add day 13 template

---
+++ d13/ac.py
@@ -0,0 +1,21 @@
+from utils import parse_input
+
+DAY = 13
+
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+def main(input_file):
+    data = parse_input(input_file)
+    print(f'Part1: {part1(data)}')
+    print(f'Part2: {part2(data)}')
+
+
+if __name__ == '__main__':
+    main(f'd{DAY}/data/input.txt')
