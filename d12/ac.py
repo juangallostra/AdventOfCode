e9368d77229923b9671ba8bbeae92b11365ec032
Juan Gallostra <juangallostra@gmail.com>
2021-12-16 09:44:01 +0100

    Add day 12 part 2

--- d12/ac.py
+++ d12/ac.py
@@ -1,8 +1,13 @@
 from utils import parse_input
+from collections import Counter
 
 DAY = 12
 
+# Part 1
 # All paths from start to end. Big caves (CAPS) can be visited more than once, small caves only once.
+
+# Part 2
+# a single small cave (different from start and end) can be visited twice.
 
 def get_connections(data):
     """Build a graph of all the connections between the rooms."""
@@ -16,9 +21,12 @@ def get_connections(data):
         if end in connections.keys():
             connections[end].append(start)
         else:
-            connections[end] = [start] 
+            connections[end] = [start]
     return connections
 
+def already_duplicate_small_room(path):
+    counts = Counter(path)
+    return any(counts[k] > 1 for k in [k for k in counts.keys() if (k.islower() and k not in ['start', 'end'])])
 
 def part1(data):
     current = 'start'
@@ -33,19 +41,37 @@ def part1(data):
         candidates = connections[current_path[-1]]
         # process each candidate room
         for room in candidates:
-            # valid candidate: CAPS room or unvisited small room. If valid, add to path 
+            # valid candidate: CAPS room or unvisited small room. If valid, add to path
             if room.isupper() or (room.islower() and room not in current_path):
+                if room == target:
+                    valid_paths.append(current_path + [room])
+                # Add a new path to explore
+                else:
+                    paths_to_explore.append(current_path + [room]) 
+    return len(valid_paths)
+
+
+def part2(data):
+    current = 'start'
+    target = 'end'
+    connections = get_connections(data)
+    valid_paths = []
+    paths_to_explore = [[current]]
+    while paths_to_explore:
+        # process next path
+        current_path = paths_to_explore.pop(0)
+        # grow path. Get next step candidates
+        candidates = connections[current_path[-1]]
+        # process each candidate room
+        for room in candidates:
+            # valid candidate: CAPS room, unvisited small room or small room and no repeated small room. If valid, add to path
+            if room != 'start' and (room.isupper() or (room.islower() and room not in current_path) or not already_duplicate_small_room(current_path)):
                 if room == target:
                     valid_paths.append(current_path + [room])
                 # Add a new path to explore
                 else:
                     paths_to_explore.append(current_path + [room])
     return len(valid_paths)
-
-
-
-def part2(data):
-    pass
 
 
 def main(input_file):
--- d12/tests.py
+++ d12/tests.py
@@ -25,12 +25,20 @@ class AOCTests(unittest.TestCase):
 
     def test_part2(self):
         # Given
-        input = parse_input(f'd{DAY}/data/test_input.txt')
-        expected_sol = parse_input(f'd{DAY}/data/test_sol_2.txt', to_int=True, single_value=True)
+        inputs = parse_input(f'd{DAY}/data/test_input.txt')
+        final_inp = []
+        current_inp = []
+        for a in inputs:
+            if (a == ''):
+                final_inp.append(current_inp)
+                current_inp = []
+            else:
+                current_inp.append(a)
+        expected_sols = parse_input(f'd{DAY}/data/test_sol_2.txt', to_int=True)
         # When
-        result = part2(input)
-        # Then
-        self.assertEqual(expected_sol, result)
+        for i in range(len(final_inp)):
+            # Then
+            self.assertEqual(part2(final_inp[i]), expected_sols[i])
 
 
 if __name__ == '__main__':
