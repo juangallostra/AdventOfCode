42f5dca5291488ae9056f56d3e0ba9f785b04fe3
Juan Gallostra <juangallostra@gmail.com>
2021-12-20 10:00:44 +0100

    Simplify Path class, full path is not required

--- d15/ac.py
+++ d15/ac.py
@@ -9,6 +9,19 @@ def add_node_to_path(path, coord):
     return new_path
 
 
+def get_node_at_position(data, x, y):
+    """
+    There is no need to compute the whole map, knowing x and y the score can be computed
+    """
+    x_offset = x // len(data[0])  # multiplier for x
+    y_offset = y // len(data)     # multiplier for y
+    cell_x = x % len(data[0])  # offset for x
+    cell_y = y % len(data)    # offset for y
+    abs_score = data[cell_y][cell_x] + x_offset + y_offset
+    score = abs_score % 9 if abs_score % 9 != 0 else 9
+    return Node(x, y, score)
+
+
 class Node:
     def __init__(self, x, y, score) -> None:
         self._x = x
@@ -27,38 +40,33 @@ class Node:
     def y(self):
         return self._y
 
+    def __repr__(self) -> str:
+        return f'Last node: ({self.x}, {self.y}); Score: {self.score})'
+
 
 class Path():
-    def __init__(self, path=None) -> None:
-        if path is None:
-            self._path = []
-        else:
-            self._path = path
+    """
+    This path only keeps track of its last node and the current score as
+    opposed to the whole path information
+    """
+
+    def __init__(self, node, score) -> None:
+        self._node = node
+        self._score = score
 
     @property
     def score(self):
-        return sum([node.score for node in self._path])
+        return self._score
 
     @property
-    def path_endpoint(self):
-        return self._path[-1]
+    def node(self):
+        return self._node
 
-    @property
-    def path(self):
-        return self._path
+    def update_path_endpoint(self, node):
+        return Path(Node(node.x, node.y, node.score), self._score + node.score)
 
-    def add_node(self, node):
-        self._path.append(node)
-
-    def add_node_from_raw_values(self, x, y, score):
-        self.add_node(Node(x, y, score))
-
-    @property
-    def coords(self):
-        return [(node.x, node.y) for node in self._path]
-
-    def clone(self):
-        return Path([node for node in self._path])
+    def __repr__(self) -> str:
+        return f'Path: ({self._node.x}, {self._node.y}), {self.score})'
 
     # Compare paths based on score
     def __lt__(self, other):
@@ -78,21 +86,17 @@ class Path():
 
     def __ne__(self, other):
         return self.score != other.score
-    
-    def __repr__(self):
-        return ','.join([f'({node.x}, {node.y})' for node in self._path])
-
 
 
 DIRECTIONS = ((1, 0), (0, 1), (-1, 0), (0, -1))
 
 
 def part1(data):
-    data = [[int(i) for i in row] for row in data] # map data
-    init = Node(0, 0, 0) # initial node
+    data = [[int(i) for i in row] for row in data]  # map data
+    init = Node(0, 0, 0)  # initial node
     target = (len(data[0])-1, len(data)-1)  # target coordinates (x,y)
     # candidate paths
-    paths = [Path([init])]  # this list has to be kept sorted
+    paths = [Path(init, 0)]  # this list has to be kept sorted
     # idea is to keep a sorted list of paths, and keep extending the shortest path until the end is reached
     target_reached = False
     shortest_path = None
@@ -103,19 +107,19 @@ def part1(data):
         # Extend shortest path in 4 directions
         for direction in DIRECTIONS:
             # get next node
-            next_x = path.path_endpoint.x + direction[0]
-            next_y = path.path_endpoint.y + direction[1]
+            next_x = path.node.x + direction[0]
+            next_y = path.node.y + direction[1]
             # idea: keep track of visited nodes
             if next_x < 0 or next_x >= len(data[0]):
-                continue 
-            if next_y < 0  or next_y >= len(data):
+                continue
+            if next_y < 0 or next_y >= len(data):
                 continue
             if (next_x, next_y) in visited:
                 continue
             node = Node(next_x, next_y, data[next_y][next_x])
             visited.append((next_x, next_y))
-            # Add to paths
-            extended_path = add_node_to_path(path, node)
+            # extend path and add to list
+            extended_path = path.update_path_endpoint(node)
             paths.append(extended_path)
             if node.x == target[0] and node.y == target[1]:
                 target_reached = True
@@ -125,15 +129,61 @@ def part1(data):
         paths = sorted(paths)
     return shortest_path.score
 
+
 def part2(data):
-    data = [[int(i) for i in row] for row in data]
-    pass
+    # There is a set of optimizations we can do here
+    # Another approach that comes to mind is to keep a track of
+    # which is the smallest score required to reach a node.
+    # We don't actually need to keep track of the path, just the last node
+    # of the path and the smallest score required to reach it. (?)
+    CELLS = 5
+    data = [[int(i) for i in row] for row in data]  # map data
+    init = Node(0, 0, 0)  # initial node
+    target = (
+        len(data[0])*CELLS-1, len(data)*CELLS-1
+    )  # target coordinates (x,y)
+    # candidate paths
+    paths = [Path(init, 0)]  # this list has to be kept sorted
+    # idea is to keep a sorted list of paths, and keep extending the shortest path until the end is reached
+    target_reached = False
+    shortest_path = None
+    visited = []
+    while not target_reached:
+        # deque shortest path
+        path = paths.pop(0)
+        # Extend shortest path in 4 directions
+        for direction in DIRECTIONS:
+            # get next node
+            next_x = path.node.x + direction[0]
+            next_y = path.node.y + direction[1]
+            # idea: keep track of visited nodes
+            if next_x < 0 or next_x >= len(data[0])*CELLS:
+                continue
+            if next_y < 0 or next_y >= len(data)*CELLS:
+                continue
+            if (next_x, next_y) in visited:
+                continue
+            node = get_node_at_position(data, next_x, next_y)
+            # node = Node(next_x, next_y, data[next_y][next_x])
+            visited.append((next_x, next_y))
+            # Add to paths
+            extended_path = path.update_path_endpoint(node)
+            # extended_path = add_node_to_path(path, node)
+            paths.append(extended_path)
+            if node.x == target[0] and node.y == target[1]:
+                target_reached = True
+                shortest_path = extended_path
+                break
+        # sort paths by score
+        # I guess this can be optimised: we can append the path at the correct position
+        paths = sorted(paths)
+    return shortest_path.score
 
 
 def main(input_file):
     data = parse_input(input_file)
     print(f'Part1: {part1(data)}')
-    print(f'Part2: {part2(data)}')
+    # print(f'Part2: {part2(data)}')
 
 
 if __name__ == '__main__':
