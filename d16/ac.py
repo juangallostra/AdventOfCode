fd32a09beb248b52d972fd06950cb0084bd3bd6c
Juan Gallostra <juangallostra@gmail.com>
2021-12-22 08:55:21 +0100

    Progress on d16

--- d16/ac.py
+++ d16/ac.py
@@ -2,8 +2,89 @@ from utils import parse_input
 
 DAY = 16
 
+# utility functions
+
+
+def hex_to_decimal(hex_str):
+    return int(hex_str, 16)
+
+
+def decimal_to_binary(x):
+    # return bin(decimal)[2:]
+    bin = ''
+    while x != 0:
+        bin = str(x % 2) + bin
+        x = x // 2
+    return ('0000' + bin)[-4:]
+
+
+def binary_to_decimal(bin_str):
+    if len(bin_str) == 1:
+        return int(bin_str)
+    return 2**(len(bin_str)-1) * int(bin_str[0]) + binary_to_decimal(bin_str[1:])
+
+
+class BITSParser():
+    # This should only parse message lengths
+    def __init__(self, message):
+        self._hex_message = message
+        self._dec_message = [hex_to_decimal(i) for i in message]
+        self._bin_message = ''.join([decimal_to_binary(a)
+                                    for a in self._dec_message])
+        self._message_to_process = self._bin_message
+
+    @property
+    def hexadecimal(self):
+        return self._hex_message
+
+    @property
+    def decimal(self):
+        return self._dec_message
+
+    @property
+    def binary(self):
+        return self._bin_message
+
+    @property
+    def message_to_process(self):
+        return self._message_to_process
+
+    def get_packets(self):
+        pass
+
+    def get_packet_length(self, pointer=0):
+        # TODO: replace _bin_message with _message_to_process
+        type_id = self._bin_message[pointer+3:pointer+6]
+        if binary_to_decimal(type_id) == 4: # Literal packet
+            end = False
+            chunks = 0
+            while not end:
+                if self._bin_message[pointer + 6 + (chunks * 5)] == '0':
+                    end = True
+                else:
+                    chunks += 1
+            return (6 + (chunks + 1) * 5) + 4 - (6 + (chunks + 1) * 5) % 4
+        else: # operator packet
+            length_type_id = int(self._bin_message[pointer + 6])
+            if length_type_id == 0:
+                data_length = binary_to_decimal(self._bin_message[pointer + 7:pointer + 7 + 15])
+                return data_length + 7 + 15 # header + id + content + actual data
+            elif length_type_id == 1:
+                num_packets = binary_to_decimal(self._bin_message[pointer + 7:pointer + 7 + 11])
+                # parse each packet
+                length = 7 + 11
+                for packet in range(num_packets): # if only info i have is num packets I guess we'll have to parse them
+                    length += self.get_next_packet(pointer)
+
+    def get_next_packet(self, pointer=0):
+        length = self.get_packet_length(pointer)
+        self._message_to_process = self._message_to_process[pointer+length:]
+        return self._bin_message[pointer:pointer+length]
+
 
 def part1(data):
+    message_parser = BITSParser(data)
+
     pass
 
 
--- d16/tests.py
+++ d16/tests.py
@@ -1,28 +1,50 @@
 
 import unittest
-from d16.ac import  part1, part2
+from d16.ac import part1, part2, BITSParser
 from utils import parse_input
 
 DAY = 16
 
-class AOCTests(unittest.TestCase):
-    def test_part1(self):
-        # Given
-        input = parse_input(f'd{DAY}/data/test_input.txt')
-        expected_sol = parse_input(f'd{DAY}/data/test_sol_1.txt', to_int=True, single_value=True)
-        # When
-        result = part1(input)
-        # Then
-        self.assertEqual(expected_sol, result)
 
-    def test_part2(self):
-        # Given
-        input = parse_input(f'd{DAY}/data/test_input.txt')
-        expected_sol = parse_input(f'd{DAY}/data/test_sol_2.txt', to_int=True, single_value=True)
-        # When
-        result = part2(input)
-        # Then
-        self.assertEqual(expected_sol, result)
+class AOCTests(unittest.TestCase):
+    def test_single_literal_message(self):
+        a = BITSParser('D2FE28')
+        packet = a.get_next_packet()
+        self.assertEqual(packet, '110100101111111000101000')
+
+    def test_parser(self):
+        a = BITSParser('D2FE28D2FE28')
+        packet = a.get_next_packet()
+        self.assertEqual(packet, '110100101111111000101000')
+        self.assertEqual(a.message_to_process, '110100101111111000101000')
+
+    def test_operator_packet(self):
+        a = BITSParser('38006F45291200')
+        self.assertEqual(a.binary, '00111000000000000110111101000101001010010001001000000000')
+
+    def test_operator_packet_2(self):
+        a = BITSParser('EE00D40C823060')
+        self.assertEqual(a.binary, '11101110000000001101010000001100100000100011000001100000')
+
+    # def test_part1(self):
+    #     # Given
+    #     input = parse_input(f'd{DAY}/data/test_input.txt')
+    #     expected_sol = parse_input(
+    #         f'd{DAY}/data/test_sol_1.txt', to_int=True, single_value=True)
+    #     # When
+    #     result = part1(input)
+    #     # Then
+    #     self.assertEqual(expected_sol, result)
+
+    # def test_part2(self):
+    #     # Given
+    #     input = parse_input(f'd{DAY}/data/test_input.txt')
+    #     expected_sol = parse_input(
+    #         f'd{DAY}/data/test_sol_2.txt', to_int=True, single_value=True)
+    #     # When
+    #     result = part2(input)
+    #     # Then
+    #     self.assertEqual(expected_sol, result)
 
 
 if __name__ == '__main__':
