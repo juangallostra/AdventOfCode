57e5c7d5f9228e33a7c97e421da8f748d919f9b3
Juan Gallostra <juangallostra@gmail.com>
2021-12-25 12:09:18 +0100

    Update

--- d17/ac.py
+++ d17/ac.py
@@ -79,6 +79,11 @@ def find_x_vels(optimal_x_vel, target):
 
 
 def part1(data):
+    # TODO: This solution is overkill and pretty much suboptimal.
+    # TODO: There are many things that shouldn't be here and are not necessary
+    # TODO: Since x and y are independent, it is way easier to just compute the max y velocity 
+    # that will pass through the y target range. There will always be an x velocity that will
+    # also pass through the x target range at the same time. 
     x_accel = -1
     target = get_target(data)
     optimal_t = get_optimal_t(target[0], accel=x_accel)
@@ -125,51 +130,18 @@ def part1(data):
 
 
 def part2(data):
-    x_accel = -1
-    target = get_target(data)
-    optimal_t = get_optimal_t(target[0], accel=x_accel)
-    # find viable t closest to optimal t
-    optimal_x_vel = -x_accel * optimal_t
-    x_vel = find_x_vels(optimal_x_vel, target[0])
-    # now that i know how much time i have available and the target y positions, compute optimal y velocity?
-    min_vels = []
-    for t in [c[1] for c in x_vel]:
-        # just compute worst case, i.e y closer to 0
-        y = sorted(target[1], key = lambda x: abs(x))[0]
-        # find min y_0 to reach target at t. However, we can do better
-        # i guess we don't care about x_vels because they end up being all 0 (?)
-        # should we keep track of them?
-        min_vels.append([t, math.ceil((y - (1/2)*(-1)*t**2)/t)])
-    # grow y until target is not reached
-    count = 0
-    # target = [target[0], sorted(target[1], key = lambda x: abs(x))]
-    for vel_set in min_vels:
-        vel_y = vel_set[1]
-        keep_trying = True
-        i = 0
-        # while keep_trying:
-        # as always, bruteforce solution...
-        while i < 1000:
-            target_hit_or_overshot = False
-            pos = [0, 0]
-            vel = [vel_set[0], vel_y]
-            i+=1
-            while not target_hit_or_overshot:
-                pos, vel = update_pos_and_vel(pos, vel)
-                if target[1][0] <= pos[1] <= target[1][1]:
-                    count += 1
-                    # print( target[1][0],pos[1],target[1][1])
-                    target_hit_or_overshot = True
-                    # if final_vel < vel_y:
-                    #     final_vel = vel_y
-                    # print(pos, vel, vel_y)
-                    vel_y += 1
-                elif pos[1] <= target[1][0]:
-                    target_hit_or_overshot = True
-                    vel_y += 1
-                    # keep_trying = False
-    # print(final_vel)
-    return count
+    pass
+    # x_accel = -1
+    # target = get_target(data)
+    # # optimal_t = get_optimal_t(target[0], accel=x_accel)
+    # # find viable t closest to optimal t
+    # # optimal_x_vel = -x_accel * optimal_t
+    # x_vel = find_x_vels(optimal_x_vel, target[0])
+    # print(x_vel)
+    # # now that i know how much time i have available and the target y positions, 
+    # # compute y velocities that hit the target at the same time that any x_velocity?
+    # x_times = set([c[1] for c in x_vel])
+    # print(x_times)
 
 
 
