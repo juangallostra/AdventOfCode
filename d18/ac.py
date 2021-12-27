ff31d68c2a4700f3bb147513665e91a0a0886ad9
Juan Gallostra <juangallostra@gmail.com>
2021-12-27 10:29:34 +0100

    Add explode and split functions

--- d18/ac.py
+++ d18/ac.py
@@ -1,4 +1,5 @@
 from utils import parse_input
+import math
 
 DAY = 18
 
@@ -6,21 +7,51 @@ def flatten(data):
     nums = str(data).replace('[', '').replace(']', '').replace(' ', '')
     return [int(n) for n in nums.split(',')]
 
+def try_get_number(num):
+    try:
+        return int(num)
+    except:
+        return None
+
 def explode_sn(data):
-    print(flatten(data))
-    exploded = []
-    for i, l1 in enumerate(data):
-        if isinstance(l1, list):
-            for j, l2 in enumerate(l1):
-                if isinstance(l2, list):
-                    for k, l3 in enumerate(l2):
-                        if isinstance(l3, list):
-                            for m, l4 in enumerate(l3):
-                                # 4 level nested data
-                                print(l4) # this explodes
+    data = str(data).replace(' ', '')
+    open_brackets = 0
+    last_number = (None, 0)
+    # problem of working with strings is that 
+    # I have to rebuild the expression?
+    for i, d in enumerate(data):
+        if d == '[':
+            open_brackets += 1
+            if open_brackets == 5: # ouuuh explosion
+                explosive = eval(data[i:i+5])
+                data = data[0:i] + '0' + data[i+5:] # replace exploding by 0
+                # get previous number, add first item of exploded pair and replace
+                if last_number[0] is not None:
+                    data = data[0:last_number[1]] + str(last_number[0] + explosive[0]) + data[last_number[1]+1:]
+                # get next number
+                for j, p in enumerate(data[i+1:]):
+                    if try_get_number(p):
+                        next_number = (try_get_number(p), i+1+j)
+                        data = data[0:next_number[1]] + str(next_number[0] + explosive[1]) + data[next_number[1]+1:]
+                        break
+                return eval(data)
+        elif d == ']':
+            open_brackets -= 1
+        elif try_get_number(d):
+                last_number = (try_get_number(d), i)
 
 def split_sn(data):
-    pass
+    # eaaaaasy
+    nums = flatten(data)
+    data = str(data).replace(' ', '')
+    for num in nums:
+        if num > 9:
+            # str([math.floor(num/2),math.ceil(num/2)]))
+            idx = data.find(str(num))
+            data = data[0:idx] + str([math.floor(num/2),math.ceil(num/2)]) + data[idx+len(str(num)):]
+            break
+    return eval(data)
+
 
 def part1(data):
     pass
--- d18/tests.py
+++ d18/tests.py
@@ -1,6 +1,6 @@
 
 import unittest
-from d18.ac import  part1, part2, explode_sn
+from d18.ac import  part1, part2, explode_sn, split_sn
 from utils import parse_input
 
 DAY = 18
@@ -14,37 +14,55 @@ class AOCTests(unittest.TestCase):
         e3 = [[6,[5,[4,[3,2]]]],1]
         e4 = [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]
         e5 = [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]
+        e6 = [[3,[2,[8,0]]],[9,[5,[4,[8,2]]]]]
+        e7 = [[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]
         # When
         r1 = explode_sn(e1)
         r2 = explode_sn(e2)
         r3 = explode_sn(e3)
         r4 = explode_sn(e4)
         r5 = explode_sn(e5)
+        r6 = explode_sn(e6)
+        r7 = explode_sn(e7)
+        r8 = explode_sn(r7)
         # Then
         self.assertEqual(r1, [[[[0,9],2],3],4])
         self.assertEqual(r2, [7,[6,[5,[7,0]]]])
         self.assertEqual(r3, [[6,[5,[7,0]]],3])
         self.assertEqual(r4, [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]])
         self.assertEqual(r5, [[3,[2,[8,0]]],[9,[5,[7,0]]]])
+        self.assertEqual(r6, [[3,[2,[8,0]]],[9,[5,[12,0]]]])
+        self.assertEqual(r7, [[[[0,7],4],[7,[[8,4],9]]],[1,1]])
+        self.assertEqual(r8, [[[[0,7],4],[15,[0,13]]],[1,1]])
 
+    def test_split(self):
+        # given
+        s1 = [[[[0,7],4],[15,[0,13]]],[1,1]]
+        # when
+        r1 = split_sn(s1)
+        r2 = split_sn(r1)
+        # then
+        self.assertEqual(r1, [[[[0,7],4],[[7,8],[0,13]]],[1,1]])
+        self.assertEqual(r2, [[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]])
+    
 
-    def test_part1(self):
-        # Given
-        input = parse_input(f'd{DAY}/data/test_input.txt')
-        expected_sol = parse_input(f'd{DAY}/data/test_sol_1.txt', to_int=True, single_value=True)
-        # When
-        result = part1(input)
-        # Then
-        self.assertEqual(expected_sol, result)
+    # def test_part1(self):
+    #     # Given
+    #     input = parse_input(f'd{DAY}/data/test_input.txt')
+    #     expected_sol = parse_input(f'd{DAY}/data/test_sol_1.txt', to_int=True, single_value=True)
+    #     # When
+    #     result = part1(input)
+    #     # Then
+    #     self.assertEqual(expected_sol, result)
 
-    def test_part2(self):
-        # Given
-        input = parse_input(f'd{DAY}/data/test_input.txt')
-        expected_sol = parse_input(f'd{DAY}/data/test_sol_2.txt', to_int=True, single_value=True)
-        # When
-        result = part2(input)
-        # Then
-        self.assertEqual(expected_sol, result)
+    # def test_part2(self):
+    #     # Given
+    #     input = parse_input(f'd{DAY}/data/test_input.txt')
+    #     expected_sol = parse_input(f'd{DAY}/data/test_sol_2.txt', to_int=True, single_value=True)
+    #     # When
+    #     result = part2(input)
+    #     # Then
+    #     self.assertEqual(expected_sol, result)
 
 
 if __name__ == '__main__':
